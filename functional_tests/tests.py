fe76cfba75c7e8f00fd2603e5ccc0058006869f0
hspandher <hspandher@outlook.com>
2014-05-05 12:11:01 +0530

    last

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -49,7 +49,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-
+        import time; time.sleep(20)
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
